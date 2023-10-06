5efa8a69cf95427b554ad40fd843d464468dd851
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-10-06 14:13:04 +0700

    Update SelectionExp101.java

--- week6/SelectionExp101.java
+++ week6/SelectionExp101.java
@@ -7,17 +7,12 @@ public class SelectionExp101 {
         System.out.println("Input year ");
         int year = input01.nextInt();
 
-        if (year % 4 == 0)
-            if (year % 100 != 0)
-            System.out.println("Leap year");
+        if (year%4 == 0)
+            if (year%100 != 0)
+                System.out.println("Leap year");
         else
-            if (year % 400 == 0)
-            System.out.println("Leap year");
-            else
             System.out.println("Not a leap year");
-        else
-        System.out.println("Not a leap year");
             
-        input01.close();
+        
     }
 }
