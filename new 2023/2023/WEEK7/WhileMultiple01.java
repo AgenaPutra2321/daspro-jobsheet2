d5daf6a1192000c5c242c8b657cdab925aadc16b
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-10-13 15:24:40 +0700

    Update WhileMultiple01.java

--- new 2023/2023/WEEK7/WhileMultiple01.java
+++ new 2023/2023/WEEK7/WhileMultiple01.java
@@ -14,14 +14,11 @@ public class WhileMultiple01 {
         while (i<=50) {
             if(i%multiple == 0 ){
                 sum = sum + i;
-                counter++;
-              
+                counter++;              
             }
             i++;
-
         }
         System.out.printf("There are %d numbers that are multiple of %d in range 1 to 50.\n",counter,multiple);
         System.out.printf("The sum from all multiples of %d in range 1 s.d. 50 is %d.\n",multiple,sum);
-
      }
 }
