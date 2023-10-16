dd318505511fa45d7cfec86390abe4488a344415
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-10-16 22:46:31 +0700

    Update DoWhileLeaveEntitlement01.java

--- new 2023/2023/WEEK7/DoWhileLeaveEntitlement01.java
+++ new 2023/2023/WEEK7/DoWhileLeaveEntitlement01.java
@@ -6,30 +6,27 @@ public class DoWhileLeaveEntitlement01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-    int leaveEntitlement, numLeave;
-    String confirmation;
-    
-    System.out.println("Jatah cuti:");
-    leaveEntitlement = sc.nextInt();
+        int leaveEntitlement, numLeave;
+        String confirmation;
 
-    do {
-        System.out.print("Apakah Anda ingin mengambil cuti(y/t)? ");
-        confirmation = sc.next();
+        System.out.println("Jatah cuti:");
+        leaveEntitlement = sc.nextInt();
 
-        if (confirmation.equalsIgnoreCase("y")) {
-            System.out.print( "Jumlah hari: ");
-            numLeave = sc.nextInt();
+        do {
+            System.out.print("Apakah Anda ingin mengambil cuti(y/t)? ");
+            confirmation = sc.next();
 
-            if (numLeave <= leaveEntitlement){
-                leaveEntitlement -= numLeave;
-                System.out.print("Sisa jatah cuti: " + leaveEntitlement);
-            } else {
-                System.out.print("Sisa jatah cutih anda tidak mencukupi");
-                break;
+            if (confirmation.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                numLeave = sc.nextInt();
+
+                if (numLeave <= leaveEntitlement) {
+                    leaveEntitlement -= numLeave;
+                    System.out.print("Sisa jatah cuti: " + leaveEntitlement);
+                } else {
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
+                }
             }
-        }
-    } while (leaveEntitlement > 0);
-
-}
-    
+        } while (confirmation.equalsIgnoreCase("y")); // Loop until the user enters "t".
+    }
 }
