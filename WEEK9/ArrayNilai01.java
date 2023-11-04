7bf1e5f10470cbdd7528ddb57e98f3b38beeeb94
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-04 14:35:53 +0700

    Update ArrayNilai01.java

--- WEEK9/ArrayNilai01.java
+++ WEEK9/ArrayNilai01.java
@@ -12,15 +12,13 @@ public class ArrayNilai01 {
     System.out.print("Masukkan nilai akhir ke-"+i+": ");
     nilaiAkhir[i] = sc.nextInt();
 }
-
-      
-       
+  
     System.out.println("Status kelulusan mahasiswa:");
-for (int i = 0; i < nilaiAkhir.length; i++) {
+    for (int i = 0; i < nilaiAkhir.length; i++) {
     if (nilaiAkhir[i] > 70) {
-        System.out.println("Mahasiswa ke-" + i + " lulus" );
+        System.out.println("Mahasiswa ke-" + i + " lulus !" );
     } else {
-        System.out.println("Mahasiswa ke-" + i + " tidak lulus " );
+        System.out.println("Mahasiswa ke-" + i + " tidak lulus !" );
     }
 }
 
