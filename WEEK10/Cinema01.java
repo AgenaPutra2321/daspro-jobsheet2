f0bbf444226f79dd9482324609b4caf0a93812ec
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-12 01:08:52 +0700

    Update Cinema01.java

--- WEEK10/Cinema01.java
+++ WEEK10/Cinema01.java
@@ -20,8 +20,8 @@ public class Cinema01 {
         System.out.printf("%s \t %s \n", audience[3][0], audience[3][1]);
 
        System.out.println(audience.length);
-       for ( int i = 0; i < audience.length;i++) {
-            System.out.println("Lenght of row " + (i + 1) + ": " + audience[i].length);
+       for ( String[] rowAudience : audience){
+            System.out.println("Lenght of row: " + rowAudience.length);
        }
     }
 }
