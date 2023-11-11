6b5fb7637da9fe89b86a5eb5877df72d4c4c5210
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-12 02:13:57 +0700

    Create CinemaWithScanner01.java

---
+++ WEEK10/CinemaWithScanner01.java
@@ -0,0 +1,74 @@
+package WEEK10;
+import java.util.Scanner;
+
+public class CinemaWithScanner01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row, column;
+        String name, next;
+        String[][] audience = new String[4][2];
+
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Input audience data");
+            System.out.println("2. Show audience list");
+            System.out.println("3. Exit");
+
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    // Input audience data
+                    System.out.print("Enter a name: ");
+                    name = sc.nextLine();
+                    System.out.print("Enter row number: ");
+                    row = sc.nextInt();
+                    System.out.print("Enter column number: ");
+                    column = sc.nextInt();
+                    sc.nextLine(); // Consume the newline character
+
+                    // Check if the seat is already occupied
+                    if (audience[row - 1][column - 1] != null) {
+                        System.out.println("Warning: The seat is already occupied. Please choose another seat.");
+                        continue; // Restart the loop
+                    }
+
+                    audience[row - 1][column - 1] = name;
+                    break;
+
+                case 2:
+                    // Show audience list
+                    for (String[] rowAudience : audience) {
+                        for (String audienceName : rowAudience) {
+                            if (audienceName == null) {
+                                System.out.print("*** ");
+                            } else {
+                                System.out.print(audienceName + " ");
+                            }
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 3:
+                    // Exit
+                    System.out.println("Exiting the program.");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice. Please enter a valid menu option.");
+            }
+
+            System.out.print("Are there any other audiences to be added? (y/n): ");
+            next = sc.nextLine();
+
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+        sc.close();
+    }
+}
