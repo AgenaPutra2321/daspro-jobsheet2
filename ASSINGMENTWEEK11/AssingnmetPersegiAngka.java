5bbefcb5b2a1183242f1c3cc7e83fefe872bf856
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-17 09:06:23 +0700

    Update AssingnmetPersegiAngka.java

--- ASSINGMENTWEEK11/AssingnmetPersegiAngka.java
+++ ASSINGMENTWEEK11/AssingnmetPersegiAngka.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 public class AssingnmetPersegiAngka {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
         
-        
-        for (int h=0;h<5;h++) {
-            //bintang 5 kolom
-            for(int i=0;i<5;i++){
-                if(h==0 || h==4 || i==0 || i==4 )
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
+        //validasi nilai N
+        if (N < 3){
+            System.out.println("Nilai N harus minimal 3. ");
+            return;
+        }
+
+        //Mencetak pola angka
+        for(int i = 1; i <= N;i++){
+            for(int j = 1; j <= N; j++) {
+
+                if (i == 1 || i == N || j == 1 || j == N) {
+                    System.out.print(N + " ");
+                } else {
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
