bc7b3d8f5bf0c258e73a292afe340351299629c4
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-17 08:41:00 +0700

    Create AssingmentSegitigaBintnag.java

---
+++ ASSINGMENTWEEK11/AssingmentSegitigaBintnag.java
@@ -0,0 +1,16 @@
+package ASSINGMENTWEEK11;
+
+import java.util.Scanner;
+
+public class AssingmentSegitigaBintnag {
+    public static void main(String[] args) {
+        for(int h=0;h<5;h++){
+
+            for(int i=0;i<(5-h);i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
