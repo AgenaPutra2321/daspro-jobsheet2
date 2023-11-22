6223bba019be2142349e1fc34fc8b6ad4910d449
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-23 00:26:37 +0700

    Update PersegiAngka.java

--- prac/PersegiAngka.java
+++ prac/PersegiAngka.java
@@ -4,15 +4,9 @@ import java.util.Scanner;
 
 public class PersegiAngka {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan nilai N : ");
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
+      int arrInt[] = {2,3,5,7,11,13,17,19,23};
+      for ( itn i=0;i<arrInt.length/2;i++){
+        System.out.print(arrInt[i+" "]);
+      }
     }
 }
