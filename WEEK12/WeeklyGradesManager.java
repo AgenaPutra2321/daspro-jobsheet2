da794942552dbc7605cb2d01bfaacd3ca7b0b4e5
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-24 16:30:41 +0700

    Update WeeklyGradesManager.java

--- WEEK12/WeeklyGradesManager.java
+++ WEEK12/WeeklyGradesManager.java
@@ -2,29 +2,6 @@ package WEEK12;
 import java.util.Scanner;
 
 public class WeeklyGradesManager {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Input the number of students and weeks
-        System.out.print("Enter the number of students: ");
-        int numStudents = scanner.nextInt();
-        System.out.print("Enter the number of weeks: ");
-        int numWeeks = scanner.nextInt();
-
-        // Create a 2D array to store grades
-        int[][] grades = new int[numStudents][numWeeks];
-        // Input students' grades
-        inputGrades(grades, numStudents, numWeeks, scanner);
-        // Display all student grades
-        displayAllGrades(grades, numStudents, numWeeks);
-        // Find the week with the highest grade from all students
-        int highestWeek = findHighestWeek(grades, numStudents, numWeeks);
-        System.out.println("Week with the highest grade from all students: Week " + (highestWeek + 1));
-        // Find the student with the highest grade
-        findStudentWithHighestGrade(grades, numStudents, numWeeks);
-
-        scanner.close();
-    }
 
     // Function to input students' grade data
     public static void inputGrades(int[][] grades, int numStudents, int numWeeks, Scanner scanner) {
@@ -89,7 +66,30 @@ public class WeeklyGradesManager {
         System.out.println("Student with the highest grade is Student " + (highestStudent + 1) +
                 " with a total grade of " + maxGrade);
     }
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Input the number of students and weeks
+        System.out.print("Enter the number of students: ");
+        int numStudents = scanner.nextInt();
+        System.out.print("Enter the number of weeks: ");
+        int numWeeks = scanner.nextInt();
+
+        // Create a 2D array to store grades
+        int[][] grades = new int[numStudents][numWeeks];
+        // Input students' grades
+        inputGrades(grades, numStudents, numWeeks, scanner);
+        // Display all student grades
+        displayAllGrades(grades, numStudents, numWeeks);
+        // Find the week with the highest grade from all students
+        int highestWeek = findHighestWeek(grades, numStudents, numWeeks);
+        System.out.println("Week with the highest grade from all students: Week " + (highestWeek + 1));
+        // Find the student with the highest grade
+        findStudentWithHighestGrade(grades, numStudents, numWeeks);
+
+        scanner.close();
+  }
+  
+}
     
 
