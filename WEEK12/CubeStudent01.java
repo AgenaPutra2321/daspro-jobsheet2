15bdddca0e12afd337f8a3054566e1acb36ffd13
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-24 12:41:41 +0700

    Update CubeStudent01.java

---
+++ WEEK12/CubeStudent01.java
@@ -0,0 +1,13 @@
+package WEEK12;
+
+public class CubeStudent01 {
+    public static double calculateArea(double side) {
+        return 6 * side * side;
+    }
+
+    public static double calculateVolume(double side) {
+        return side * side * side;
+    }
+}
+       
+
