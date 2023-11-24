00e017da96dfc797d3bf0a7768af72d0f7ded307
AgenaPutra2321 <agnaputra2321@gmail.com>
2023-11-24 16:30:37 +0700

    Create Weekly.java

---
+++ WEEK12/Weekly.java
@@ -0,0 +1,11 @@
+package WEEK12;
+
+public class Weekly {
+    public static void main(String[] args) {
+        int [][] grades = new int [5][7];
+    }
+    static void input(int[][] data) {
+        
+    }
+    
+}
